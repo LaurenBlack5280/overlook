6a02c4317d8bf9fae04ed143032132dd18fd81cf
Lauren Black <laurenblack5280@gmail.com>
2022-11-11 19:24:40 -0700

    create User tests, create User class, pass tests

---
+++ test/User-test.js
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import User from '../src/classes/User.js'
+
+describe('User', function() {
+let userOne, userTwo
+  beforeEach(() => {
+    const user1 = {
+    id: 1,
+    name: "Leatha Ullrich"
+    }
+
+    const user2 = {
+    id: 2,
+    name: "Rocio Schuster"
+    }
+
+      userOne = new User(user1)
+      userTwo = new User(user2)
+  })
+
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+  });
+
+  it('should have an id', function() {
+    expect(userOne.id).to.equal(1);
+    expect(userTwo.id).to.equal(2);
+  });
+
+  it('should have a name', function() {
+    expect(userOne.name).to.equal('Leatha Ullrich');
+    expect(userTwo.name).to.equal('Rocio Schuster');
+  });
+});
